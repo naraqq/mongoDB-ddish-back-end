a1b4688d14855d7bac91c47c0a446e96d842003e
narmandakh.g <narmandakh.g@ddishtv.mn>
2022-08-16 20:27:49 +0800

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 
 
 const MongoClient = require('mongodb').MongoClient
-const url = 'mongodb://192.168.0.13:27017'
+const url = 'mongodb://127.0.0.1:27017'
 const dbName = 'test'
 let db
 
